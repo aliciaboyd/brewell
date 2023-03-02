645f8794c4d979e90eed2f559c348b4ae6aa02a0
Alicia Boyd <26505495+aliciaboyd@users.noreply.github.com>
2023-03-02 00:20:03 -0500

    Adds mobile menu styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,22 @@
 const nav = document.querySelector(".main-nav");
+const menuList = document.querySelector(".main-nav ul");
+const menuBtn = document.querySelector("#nav-btn");
+const hero = document.querySelector(".hero");
+const hero_height = hero.offsetHeight * .8;
+let scrollPosition = window.scrollY;
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 600 ||
-    document.documentElement.scrollTop > 600
-  ) {
-    nav.classList.add("scroll");
+window.onscroll = () => {
+  let scrollPosition = window.scrollY;
+
+  if (scrollPosition > hero_height) {
+    nav.classList.add("scroll")
   } else {
-    nav.classList.remove("scoll");
+    nav.classList.remove("scroll")
   }
 }
 
-window.onscroll = function () {
-  scrollFunction();
-};
+menuBtn.addEventListener("click", showMenu);
+
+function showMenu() {
+  menuList.classList.toggle("active");
+}
